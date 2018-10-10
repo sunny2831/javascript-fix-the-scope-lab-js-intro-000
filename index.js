036c5a28686ccfafd79b65c51dc1efb48d9934f0
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-10 17:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n) {
-  return n + two
+var n = 973
 
-  // Feel free to move things around!
-  const two = 2
+function add2(n) {
+   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
